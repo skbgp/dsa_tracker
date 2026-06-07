2a5d585cce3dbaca04cbf31d0393ae6a6dcc4136
Shubham Kumar <krshubham590@gmail.com>
2026-06-07 22:49:02 +0530

    Revert broken modularization, restore working script.js with admin+grouping features

--- js/main.js
+++
@@ -1,8 +0,0 @@
-import './globals.js';
-import './monaco-setup.js';
-import './utils.js';
-import './auth.js';
-import './drafts.js';
-import './autocomplete.js';
-import './tracker.js';
-import './wiki.js';
